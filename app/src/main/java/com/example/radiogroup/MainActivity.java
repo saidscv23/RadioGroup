eb0eb3926cb90550c94898e4e25b5434b45983c3
saidscv23 <said.carrillo438@ist17dejulio.edu.ec>
2024-01-10 11:01:05 -0500

    Final

--- app/src/main/java/com/example/radiogroup/MainActivity.java
+++ app/src/main/java/com/example/radiogroup/MainActivity.java
@@ -1,18 +1,20 @@
 package com.example.radiogroup;
-
-import android.graphics.Color;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RadioButton;
+import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RadioGroup radioGroupTheme;
+    private RadioGroup radioGroupOperation;
     private Button button;
     private RelativeLayout mainLayout;
 
@@ -21,21 +23,69 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        radioGroupTheme = findViewById(R.id.radioGroupTheme);
+        radioGroupOperation = findViewById(R.id.radioGroupOperation);
         button = findViewById(R.id.button);
-        mainLayout = findViewById(R.id.mainLayout); // Cambia a R.id.mainLayout
+        mainLayout = findViewById(R.id.mainLayout);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int selectedId = radioGroupTheme.getCheckedRadioButtonId();
+                int selectedId = radioGroupOperation.getCheckedRadioButtonId();
 
-                if (selectedId == R.id.radioButtonOscuro) {
-                    mainLayout.setBackgroundColor(Color.BLACK);
-                } else if (selectedId == R.id.radioButtonClaro) {
-                    mainLayout.setBackgroundColor(Color.WHITE);
+                if (selectedId == R.id.radioButtonSuma) {
+                    showInputDialog("Suma");
+                } else if (selectedId == R.id.radioButtonResta) {
+                    showInputDialog("Resta");
                 }
             }
         });
+    }
+
+    private void showInputDialog(final String operation) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(operation);
+
+        // Inflar el diseño personalizado del cuadro de diálogo
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.dialog_input, null);
+        builder.setView(dialogView);
+
+        final EditText editTextNumber1 = dialogView.findViewById(R.id.editTextNumber1);
+        final EditText editTextNumber2 = dialogView.findViewById(R.id.editTextNumber2);
+
+        builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                String strNumber1 = editTextNumber1.getText().toString();
+                String strNumber2 = editTextNumber2.getText().toString();
+
+
+                int number1 = Integer.parseInt(strNumber1);
+                int number2 = Integer.parseInt(strNumber2);
+
+
+                if (operation.equals("Suma")) {
+                    int result = number1 + number2;
+                    showToast("Resultado de la suma: " + result);
+                } else if (operation.equals("Resta")) {
+                    int result = number1 - number2;
+                    showToast("Resultado de la resta: " + result);
+                }
+            }
+        });
+
+        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                // Acciones al hacer clic en "Cancelar"
+            }
+        });
+
+        builder.show();
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
